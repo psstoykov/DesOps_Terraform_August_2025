29eef4dda1033049f7b0c262281292dd9ba65a93
Petar Stoykov <145485240+psstoykov@users.noreply.github.com>
2025-08-19 10:35:26 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,10 @@
 variable "resource_group_name" {
   description = "The name of the resource group where resources will be created."
   type        = string
-
-
 }
 variable "location" {
   description = "The Azure Region where the resources will be created."
   type        = string
-
-
 }
+
 
