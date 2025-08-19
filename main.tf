0a9d2f799f4152be5b8db90e2fb93eab442e9ddc
psstoykov <145485240+psstoykov@users.noreply.github.com>
2025-08-19 11:49:12 +0200

    add posibility to store the state file

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.40.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragepeshy"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
